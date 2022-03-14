c1a9992bb4e040a923b43c641d7daa48a92c4cb5
최유선 <choiyuseon@choeyuseon-ui-MacBookPro.local>
2022-03-15 00:27:23 +0900

    TMAP connect

--- app/src/main/java/com/example/mapdemo/MainActivity.kt
+++ app/src/main/java/com/example/mapdemo/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.mapdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.mapdemo.databinding.ActivityMainBinding
 import com.example.mapdemo.model.LocationLatLngEntity
 import com.example.mapdemo.model.SearchResultEntity
+import com.example.mapdemo.response.search.Poi
+import com.example.mapdemo.response.search.Pois
+import com.example.mapdemo.utility.RetrofitUtil
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: SearchRecyclerAdapter
@@ -18,10 +29,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        job = Job()
+
         initAdapter()
         initViews()
+        bindViews()
         initData()
-        setData()
+//        setData()
     }
 
     private fun initViews() = with(binding) {
@@ -33,22 +47,66 @@ class MainActivity : AppCompatActivity() {
         adapter = SearchRecyclerAdapter()
     }
 
+    private fun bindViews() = with(binding) {
+        searchButton.setOnClickListener {
+            searchKeyword(searchBarInputView.text.toString())
+        }
+    }
+
     private fun initData() {
         adapter.notifyDataSetChanged()
     }
 
-    private fun setData() {
-        val dataList = (0..10).map{
+    private fun setData(pois: Pois) {
+        val dataList = pois.poi.map {
             SearchResultEntity(
-                name = "빌딩 $it",
-                fullAdress = "주소 $it",
-                locationLatLng = LocationLatLngEntity(
-                    it.toFloat(),
-                    it.toFloat()
-                )
+                fullAdress = makeMainAdress(it),
+                name = it.name ?: "빌딩명 없음",
+                locationLatLng = LocationLatLngEntity(it.noorLat, it.noorLon)
             )
         }
         adapter.setSearchResultList(dataList) {
+            Toast.makeText(this, "빌딩이름 : ${it.name} 주소 : ${it.fullAdress} 위도/경도 : ${it.locationLatLng}", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun searchKeyword(keywordString: String) {
+        launch(coroutineContext) {
+            try {
+                withContext(Dispatchers.IO) {
+                    val response = RetrofitUtil.apiService.getSearchLocation(
+                        keyword = keywordString
+                    )
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        withContext(Dispatchers.Main) {
+                            Log.e("response", body.toString())
+                            body?.let {searchResponse ->
+                                setData(searchResponse.searchPoiInfo.pois)
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this@MainActivity, "검색하는 과정에서 에러가 발생했습니다. : ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun makeMainAdress(poi: Poi): String =
+        if (poi.secondNo?.trim().isNullOrEmpty()) {
+            (poi.upperAddrName?.trim() ?: "") + " " +
+                    (poi.middleAddrName?.trim() ?: "") + " " +
+                    (poi.lowerAddrName?.trim() ?: "") + " " +
+                    (poi.detailAddrName?.trim() ?: "") + " " +
+                    poi.firstNo?.trim()
+        } else {
+            (poi.upperAddrName?.trim() ?: "") + " " +
+                    (poi.middleAddrName?.trim() ?: "") + " " +
+                    (poi.lowerAddrName?.trim() ?: "") + " " +
+                    (poi.detailAddrName?.trim() ?: "") + " " +
+                    (poi.firstNo?.trim() ?: "") + " " +
+                    poi.secondNo?.trim()
+        }
 }
